3518cf38af71cfd9385e57794cc2d5a736014342
theflippantfox <theflippantfox@gmail.com>
2024-08-08 15:34:03 +0000

    updated dots

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+export PATH="/home/theflippantfox/.local/bin:$PATH"
+oh-my-posh init fish --config "/home/theflippantfox/.config/fish/themes/mojada.omp.json" | source
+
+set -g fish_greeting ''
+
+alias ls="exa -la"
+alias bat="cat"
